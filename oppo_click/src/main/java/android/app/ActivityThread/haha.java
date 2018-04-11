ec9e28ba737235552bad15f92acd852bfecf4fef
skyun1314 <626504984@qq.com>
2018-04-11 17:56:53 +0800

    haha

--- oppo_click/src/main/java/android/app/ActivityThread/haha.java
+++
@@ -1,9 +0,0 @@
-package android.app.ActivityThread;
-
-/**
- * Created by zk on 2018/1/22.
- */
-
-public class haha {
-    public String processName;
-}
--- oppo_maket/src/main/java/com/oppo/whoops/domain/dto/req/mt.java
+++
@@ -1,6 +0,0 @@
-package com.oppo.whoops.domain.dto.req;
-
-/* compiled from: IWhoopsHandler */
-public interface mt {
-    void a(String str, int i, int i2, int i3, int i4, String str2, String str3);
-}
